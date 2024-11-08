5cd0813c194cedaee79ba49e8af9cb94ef7ccccf
Yasmine <yasmine.amaddah@laplateforme.io>
2024-11-08 20:10:35 +0100

    logo added

--- AppAirbnb/resources/js/Pages/Annonces/Index.vue
+++ AppAirbnb/resources/js/Pages/Annonces/Index.vue
@@ -1,3 +1,16 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+</script>
+
 <template>
-    caca
+    <AppLayout title="Annonces">
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    caca
+                </div>
+            </div>
+        </div>
+    </AppLayout>
 </template>
